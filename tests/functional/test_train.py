49ada6e738c341cf683d95c80e8392ac41480a8a
Paxton van der Graaf <paxton@live.com>
2024-05-10 22:11:09 +0200

    updated tests

--- tests/functional/test_train.py
+++ tests/functional/test_train.py
@@ -13,7 +13,7 @@ from src.train.train import (
 from src.train.utils import load_yaml_file, get_data
 
 
-def fake_get_data(n: int = 100):
+def _fake_get_data(n: int = 100):
     # Create a fake DataFrame
     data = {
         "fraud_bool": np.random.randint(0, 2, size=n),
@@ -56,94 +56,94 @@ def fake_get_data(n: int = 100):
     return df
 
 
-def test_returns_required_parameters_from_yaml_file():
-    # Given
-    file_path = "config.yaml"
+class TestTrainer:
+    def test_returns_required_parameters_from_yaml_file(self):
+        # Given
+        file_path = "config.yaml"
 
-    # When
-    yaml_data = load_yaml_file(file_path)
+        # When
+        yaml_data = load_yaml_file(file_path)
 
-    # Then
-    assert yaml_data is not None, "Failed to load YAML file."
+        # Then
+        assert yaml_data is not None, "Failed to load YAML file."
 
-    # Given
-    required_parameters = ["seed", "target", "data_path", "train_size", "test_size"]
+        # Given
+        required_parameters = ["seed", "target", "data_path", "train_size", "test_size"]
 
-    # When
-    missing_parameters = [
-        param for param in required_parameters if param not in yaml_data
-    ]
-    # Then
-    assert (
-        not missing_parameters
-    ), f"Missing parameters: {', '.join(missing_parameters)}"
-    assert isinstance(yaml_data["seed"], int), "Seed should be an integer."
-    assert isinstance(
-        yaml_data["target"], str
-    ), "Target column name should be a string."
-    assert (
-        isinstance(yaml_data["train_size"], float) and 0 <= yaml_data["train_size"] < 1
-    ), "Train size should be a float and between 0 and 1."
-    assert (
-        isinstance(yaml_data["test_size"], float) and 0 <= yaml_data["test_size"] < 1
-    ), "Test size should be a float and between 0 and 1."
+        # When
+        missing_parameters = [
+            param for param in required_parameters if param not in yaml_data
+        ]
+        # Then
+        assert (
+            not missing_parameters
+        ), f"Missing parameters: {', '.join(missing_parameters)}"
+        assert isinstance(yaml_data["seed"], int), "Seed should be an integer."
+        assert isinstance(
+            yaml_data["target"], str
+        ), "Target column name should be a string."
+        assert (
+            isinstance(yaml_data["train_size"], float)
+            and 0 <= yaml_data["train_size"] < 1
+        ), "Train size should be a float and between 0 and 1."
+        assert (
+            isinstance(yaml_data["test_size"], float)
+            and 0 <= yaml_data["test_size"] < 1
+        ), "Test size should be a float and between 0 and 1."
 
+    def test_returns_train_holdout_test_dataframes(self):
+        # Given
+        yaml_path = "config.yaml"
+        df = _fake_get_data(1000)
 
-def test_returns_train_holdout_test_dataframes():
-    # Given
-    yaml_path = "config.yaml"
-    df = fake_get_data(1000)
+        # When
+        X_train, X_holdout, X_test, y_train, y_holdout, y_test = split_dataframes(
+            df, load_yaml_file(yaml_path)
+        )
+        full_df_length = len(df)
 
-    # When
-    X_train, X_holdout, X_test, y_train, y_holdout, y_test = split_dataframes(
-        df, load_yaml_file(yaml_path)
-    )
-    full_df_length = len(df)
+        # Then
+        assert isinstance(X_train, pd.DataFrame)
+        assert isinstance(X_holdout, pd.DataFrame)
+        assert isinstance(X_test, pd.DataFrame)
+        assert isinstance(y_train, pd.Series)
+        assert isinstance(y_holdout, pd.Series)
+        assert isinstance(y_test, pd.Series)
+        assert len(X_train) + len(X_holdout) + len(X_test) == full_df_length
 
-    # Then
-    assert isinstance(X_train, pd.DataFrame)
-    assert isinstance(X_holdout, pd.DataFrame)
-    assert isinstance(X_test, pd.DataFrame)
-    assert isinstance(y_train, pd.Series)
-    assert isinstance(y_holdout, pd.Series)
-    assert isinstance(y_test, pd.Series)
-    assert len(X_train) + len(X_holdout) + len(X_test) == full_df_length
+    def test_returns_log_reg_model_and_test_metrics(self):
+        # Given
+        yaml_path = "config.yaml"
 
+        # When
+        df = _fake_get_data(1000)
+        X_train, X_holdout, X_test, y_train, y_holdout, y_test = split_dataframes(
+            df, load_yaml_file(yaml_path)
+        )
+        model, recall, roc_auc = logreg_preprocessor_and_model(
+            X_train, X_test, y_train, y_test
+        )
 
-def test_returns_log_reg_model_and_test_metrics():
-    # Given
-    yaml_path = "config.yaml"
+        # Then
+        assert isinstance(model, Pipeline)
+        assert 0 <= recall <= 1, "Recall should be between 0 and 1."
+        assert 0 <= roc_auc <= 1, "ROC AUC should be between 0 and 1."
 
-    # When
-    df = fake_get_data(1000)
-    X_train, X_holdout, X_test, y_train, y_holdout, y_test = split_dataframes(
-        df, load_yaml_file(yaml_path)
-    )
-    model, recall, roc_auc = logreg_preprocessor_and_model(
-        X_train, X_test, y_train, y_test
-    )
+    def test_returns_lgbm_preprocessor_model_and_test_metrics(self):
+        # Given
+        yaml_path = "config.yaml"
 
-    # Then
-    assert isinstance(model, Pipeline)
-    assert 0 <= recall <= 1, "Recall should be between 0 and 1."
-    assert 0 <= roc_auc <= 1, "ROC AUC should be between 0 and 1."
+        # When
+        df = _fake_get_data()
+        X_train, X_holdout, X_test, y_train, y_holdout, y_test = split_dataframes(
+            df, load_yaml_file(yaml_path)
+        )
+        preprocessor, model, recall, roc_auc = lgbm_preprocessor_and_model(
+            X_train, X_test, y_train, y_test
+        )
 
-
-def test_returns_lgbm_preprocessor_model_and_test_metrics():
-    # Given
-    yaml_path = "config.yaml"
-
-    # When
-    df = fake_get_data()
-    X_train, X_holdout, X_test, y_train, y_holdout, y_test = split_dataframes(
-        df, load_yaml_file(yaml_path)
-    )
-    preprocessor, model, recall, roc_auc = lgbm_preprocessor_and_model(
-        X_train, X_test, y_train, y_test
-    )
-
-    # Then
-    assert isinstance(preprocessor, Pipeline)
-    assert isinstance(model, lgb.Booster)
-    assert 0 <= recall <= 1, "Recall should be between 0 and 1."
-    assert 0 <= roc_auc <= 1, "ROC AUC should be between 0 and 1."
+        # Then
+        assert isinstance(preprocessor, Pipeline)
+        assert isinstance(model, lgb.Booster)
+        assert 0 <= recall <= 1, "Recall should be between 0 and 1."
+        assert 0 <= roc_auc <= 1, "ROC AUC should be between 0 and 1."
--- tests/unit/test_db.py
+++ tests/unit/test_db.py
@@ -11,70 +11,77 @@ SCHEMA = "ml_schema"
 TABLE_LIST = ["models"]
 
 
-def tests_connection_db_schemas_tables(
-    connection_string: str = CONNECTION_STING,
-    schema_name: str = SCHEMA,
-    table_list: list[str] = TABLE_LIST,
-):
-    # Given
-    engine = create_engine(connection_string)
-    conn = engine.connect()
+class TestDatabase:
+    def tests_connection_db_schemas_tables(
+        self,
+        connection_string: str = CONNECTION_STING,
+        schema_name: str = SCHEMA,
+        table_list: list[str] = TABLE_LIST,
+    ):
+        # Given
+        engine = create_engine(connection_string)
+        conn = engine.connect()
 
-    # When
-    inspector = inspect(engine)
-    crete_database_schemas_tables(connection_string, schema_name, table_list)
-
-    # Then
-    assert database_exists(connection_string) == True
-    assert schema_name in inspector.get_schema_names()
-    assert all(
-        [table in inspector.get_table_names(schema=schema_name) for table in table_list]
-    )
-    conn.close()
-
-
-def test_insert_values(
-    connection_string: str = CONNECTION_STING,
-    schema_name: str = SCHEMA,
-):
-    # Given
-    engine = create_engine(connection_string)
-    conn = engine.connect()
-    table_name = "models"
-    values = {
-        "model_id": uuid4(),  # Generate a random UUID
-        "train_date": datetime(2024, 1, 1, 1, 1),  # Use the current date and time
-        "model_name": "test_model",
-        "model_type": "test_type",
-        "hyperparameters": {"param1": "foo", "param2": "bar"},  # Example JSON data
-        "roc_auc_train": 0.9,
-        "recall_train": 0.8,
-        "roc_auc_test": 0.7,
-        "recall_test": 0.6,
-        "model_path": "path/to/model",
-    }
-    # Start a transaction
-    with conn.begin():
         # When
-        insert_values_into_table(connection_string, schema_name, table_name, values)
+        inspector = inspect(engine)
+        crete_database_schemas_tables(connection_string, schema_name, table_list)
 
         # Then
-        # Define the table
-        metadata = MetaData()
-        table = Table(table_name, metadata, autoload_with=engine, schema=schema_name)
+        assert database_exists(connection_string) == True
+        assert schema_name in inspector.get_schema_names()
+        assert all(
+            [
+                table in inspector.get_table_names(schema=schema_name)
+                for table in table_list
+            ]
+        )
+        conn.close()
 
-        # Create a Select object
-        stmt = select(table).where(table.c.model_id == values["model_id"])
+    def test_insert_values(
+        self,
+        connection_string: str = CONNECTION_STING,
+        schema_name: str = SCHEMA,
+    ):
+        # Given
+        engine = create_engine(connection_string)
+        conn = engine.connect()
+        table_name = "models"
+        values = {
+            "model_id": uuid4(),  # Generate a random UUID
+            "train_date": datetime(2024, 1, 1, 1, 1),  # Use the current date and time
+            "model_name": "test_model",
+            "model_type": "test_type",
+            "hyperparameters": {"param1": "foo", "param2": "bar"},  # Example JSON data
+            "roc_auc_train": 0.9,
+            "recall_train": 0.8,
+            "roc_auc_test": 0.7,
+            "recall_test": 0.6,
+            "model_path": "path/to/model",
+        }
+        # Start a transaction
+        with conn.begin():
+            # When
+            insert_values_into_table(connection_string, schema_name, table_name, values)
 
-        # Execute the statement and fetch one row
-        result = conn.execute(stmt).fetchone()
-        keys = list(values)
-        result_dict = dict(zip(keys, result))
-        # Assert that the inserted data is correct
-        assert result is not None, "No result found"
-        for key, value in values.items():
-            assert result_dict[key] == value, f"Value for {key} does not match"
+            # Then
+            # Define the table
+            metadata = MetaData()
+            table = Table(
+                table_name, metadata, autoload_with=engine, schema=schema_name
+            )
 
-        # Delete the inserted row
-        delete_stmt = delete(table).where(table.c.model_id == values["model_id"])
-        conn.execute(delete_stmt)
+            # Create a Select object
+            stmt = select(table).where(table.c.model_id == values["model_id"])
+
+            # Execute the statement and fetch one row
+            result = conn.execute(stmt).fetchone()
+            keys = list(values)
+            result_dict = dict(zip(keys, result))
+            # Assert that the inserted data is correct
+            assert result is not None, "No result found"
+            for key, value in values.items():
+                assert result_dict[key] == value, f"Value for {key} does not match"
+
+            # Delete the inserted row
+            delete_stmt = delete(table).where(table.c.model_id == values["model_id"])
+            conn.execute(delete_stmt)
